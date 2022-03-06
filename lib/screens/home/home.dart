bfa83b16c53f0ac3681d14fb4f7a647d0fd559da
MarasDriver <mkamzel@hotmail.com>
2022-03-06 14:28:07 +0100

    edytor nazw/modeli

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_exmle/screens/home/widgets/car_modal.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../auth/data/providers/auth_state.dart';
+import 'data/models/cars_model.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
+  Home({Key? key}) : super(key: key);
+  List<String> documentsID = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +20,78 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: Container(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance.collection("cars").snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  List<Cars> _listOfCars =
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Cars.fromJson(data);
+                  }).toList();
+
+                  documentsID =
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    return document.id;
+                  }).toList();
+
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: _listOfCars.length,
+                      itemBuilder: (context, index) {
+                        Cars _car = _listOfCars[index];
+                        return listItem(_car, documentsID[index], context);
+                      });
+                }
+              }),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Cars data = Cars(
+            brand: "mastodont",
+            model: "V3",
+          );
+          FirebaseFirestore.instance.collection("cars").add(data.toJson());
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget listItem(Cars car, docID, context) {
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        if (direction == DismissDirection.startToEnd ||
+            direction == DismissDirection.endToStart)
+          FirebaseFirestore.instance.collection("cars").doc(docID).delete();
+      },
+      child: ListTile(
+        title: Text(car.brand!),
+        subtitle: Text(car.model!),
+        trailing: IconButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (_) => Dialog(
+                      child: CarModal(
+                        car: car,
+                        docID: docID,
+                      ),
+                    ));
+          },
+          icon: Icon(Icons.edit),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/home/widgets/car_modal.dart
@@ -0,0 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import '../data/models/cars_model.dart';
+
+class CarModal extends StatefulWidget {
+  CarModal({Key? key, this.car, this.docID}) : super(key: key);
+
+  final Cars? car;
+  final String? docID;
+
+  @override
+  State<CarModal> createState() => _CarModalState();
+}
+
+class _CarModalState extends State<CarModal> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController? _brandController = TextEditingController();
+  TextEditingController? _modelController = TextEditingController();
+  Cars? _car;
+  @override
+  void initState() {
+    _car = widget.car!;
+    _brandController = TextEditingController(text: _car!.brand);
+    _modelController = TextEditingController(text: _car!.model);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextFormField(
+                onChanged: (v) {
+                  setState(() {
+                    _car!.brand = v;
+                  });
+                },
+                controller: _brandController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter a brand";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextFormField(
+                onChanged: (v) {
+                  setState(() {
+                    _car!.model = v;
+                  });
+                },
+                controller: _modelController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Please enter a model";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("Cancel")),
+                TextButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        FirebaseFirestore.instance
+                            .collection("cars")
+                            .doc(widget.docID)
+                            .update(_car!.toJson())
+                            .whenComplete(() => Navigator.of(context).pop());
+                      }
+                    },
+                    child: Text("Save")),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
